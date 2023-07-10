d73c0ce6d1db14317bbb247774af389350cadbba
Emirhan Uluksar <uluksaremirhan@gmail.com>
2023-07-10 17:48:25 +0300

    unused using fields are deleted

--- code/backend/Hyper.Backend.Web/Infrastructure/StartupAuth.cs
+++ code/backend/Hyper.Backend.Web/Infrastructure/StartupAuth.cs
@@ -1,10 +1,3 @@
-using System.IdentityModel.Tokens.Jwt;
-using System.Security.Cryptography;
-using System.Text;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.IdentityModel.Tokens;
-
 namespace WebAPI.Infrastructure;
 
 public static class StartupAuth {
